b19e95f6b9be102dadf8345ad309d01ac70a142f
Kajetan <128511852+KAJ3TAN@users.noreply.github.com>
2025-06-20 12:24:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$env = getenv('ENV');
+echo "Aktualne środowisko: " . $env;
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+    <style>
+        body {
+            background-color: #b3e0ff; 
+            color: #222;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 20vh;
+        }
+    </style>
+</head>
+<body>
+    <h1>hello world</h1>
+</body>
+</html>
